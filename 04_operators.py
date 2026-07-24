07ba639f7dea9c5fc09ec865b8bf4aa11e7a668f
palas <palashshahare@gmail.com>
2026-07-24 19:56:45 +0530

    24-07-2026 Commit

---
+++ 04_operators.py
@@ -0,0 +1,14 @@
+# Arithematic operators
+# +, -, *, /, %
+
+#Assignment operators
+# =, +=, -=,
+
+#Comparison operators
+# ==, !=, <=, >=, <, >
+
+#Logical operator
+# AND, OR, NOT
+
+#Identiy operator
+# IS, IS NOT
