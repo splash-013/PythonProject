d90dec458c4ad76caceac36403c66f57c0975420
palash.shahare <palash.shahare@bizsense.in>
2026-07-30 11:06:56 +0530

    First commit

---
+++ 05_jumpingStTEMENTS.py
@@ -0,0 +1,12 @@
+# break and continue
+
+list = ['apple', 'orange', 3.14, 'Palash', 34.55, 350, True ]
+#print(list[0:2])
+
+# for x in list[0:2]:
+#     print(x)
+
+for x in list:
+    print(x)
+    if x == 'Palash':
+        break
--- loopingStatements.py
+++ loopingStatements.py
@@ -11,3 +11,25 @@
 #     print(i)
 #     i=i+1 #increment/decrement
 
+
+# for loop
+# for i in range(10): #indexing starts form 0 to (n-1)
+#     print(i)
+
+# for i in range(1,11):
+#     print(i)
+
+#print only even numbers between 1 to 20
+# for i in range(0,21,2):
+#     print(i)
+
+#print only odd numbers between 1 to 20
+for i in range(1,21,2):
+    print(i)
+
+
+
+
+
+
+
