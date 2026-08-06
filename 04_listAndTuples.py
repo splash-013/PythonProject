3880e820b198204478a7a8d56c20d5b24f4e9786
palas <palashshahare@gmail.com>
2026-08-06 16:47:40 +0530

    24-07-2026 Commit

---
+++ 04_listAndTuples.py
@@ -0,0 +1,21 @@
+# List are used to store a collection of values of different datatypes
+
+list = ['Palash', 3.14, 'Orange', 'Green', 33, 45.7, True]
+
+# print(list[0])
+# print(list[1:3]) #range
+
+list[2] = False #change values in a list, mutable(changeable), unlike Strings
+print(list)
+
+# String methods
+# .lower()
+# .upper()
+# .strip() #remove spaces
+# .replace()
+# .startswith()
+# .endswith()
+# .find()
+# .count()
+# .split() #convert string into list
+# .join()
