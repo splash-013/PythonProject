3a0b6573b93906b528b26406cdc579d81cb00105
palash.shahare <palash.shahare@bizsense.in>
2026-08-11 09:21:26 +0530

    Final Python commit

---
+++ playWright/firstTestCase.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page, expect
+# "From Playwright's synchronous API, give me the sync_playwright, Page, expect functionality."
+
+
+# import playwright package, then import sync or async package and then fixture (Page) class
+
+def test_verifyLoginPage(page:Page): #page fixture is part of Page class
+    page.goto("https://testautomationpractice.blogspot.com/") #.goto("") is used to pass URL
+    myURL=page.url #capture the URL, predefined property hence no page.url()
+    # IMP-A property represents information that an object already has, hence no need for ()
+    print('URL of the page: ', myURL)
+    expect(page).to_have_url("https://testautomationpractice.blogspot.com/") #expect() is used for assertion, used to verify the URL
+
+def test_verifyTitle(page:Page):
+    page.goto("https://testautomationpractice.blogspot.com/")
+    myTitle=page.title() #capture page title, predefined method hence page.title()
+    # IMP-A method represents an action/function that needs to be executed.
+    print('Page title: ',myTitle)
+    expect(page).to_have_title('Automation Testing Practice')
---
+++ playWright/launchBrowser.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+# "From Playwright's synchronous API, give me the sync_playwright (start PW to use browser) functionality."
+with sync_playwright() as p: #Start the Playwright system so that we can work with browsers, "Start/create the Playwright environment."
+
+    browser=p.chromium.launch(headless=False) #launch the Chromium browser
+    context=browser.new_context() #Create a new, isolated browser session
+    page=context.new_page() #Create a new browser tab inside that session
+    page.goto("https://testautomationpractice.blogspot.com/")
+    input('Press enter')
+
+
+
