629b113e64c646f860c1f1551b0aed5ab5a4102a
palas <palashshahare@gmail.com>
2026-08-12 22:22:02 +0530

    12-08-2026 Commit

--- practice_PlayWright/test_builtInLocators.py
+++ practice_PlayWright/test_builtInLocators.py
@@ -1,12 +1,22 @@
-from time import time
+#from time
 
 from playwright.sync_api import Page, expect
 
-# page.get_by_alt_name()
 
-def test_verifytitle(page:Page):
-    page.goto('https://demo.nopcommerce.com/')
-    #time().sleep(5) #seconds
-    #page.wait_for_timeout(5000)
-    logo=page.get_by_alt_text('nopCommerce demo store')
-    #expect(logo).to_be_visible()
+
+
+def test_verify_elements(page:Page):
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    #time.sleep(5) #seconds wait in python
+    #page.wait_for_timeout(5000) #wait in playwright
+
+    # page.get_by_alt_name(), this is used for images
+    # logo=page.get_by_alt_text('company-branding')
+    # expect(logo).to_be_visible()
+    #expect(page.get_by_alt_text('company-branding')).to_be_visible()
+
+    # page.get_by_test()
+    #expect(page.get_by_text("Login")).to_be_visible()
+
+
+
